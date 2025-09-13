715e938e64089c250a91ab522f4451763f251121
Fatehma <fatima.a13.fa@gmail.com>
2025-09-13 18:02:26 +0500

    Add files via upload
    Question: Create a tuple with 5 different fruits. Access the first and last fruit, find the length of the tuple, and check if "apple" is in the tuple.

---
+++ Intermediate-Python/Tuple fruits.py
@@ -0,0 +1,16 @@
+fruits  = ('apple','oranges','pineapple','mango','cherry')
+
+first_fruit = fruits[0]
+last_fruit = fruits[-1]
+
+length = len(fruits)
+
+has_apple = "apple" in fruits
+has_kiwi = "kiwi" in fruits
+
+print(f"Fruits tuple: {fruits}")
+print(f"First fruit: {first_fruit}")
+print(f"Last fruit: {last_fruit}")
+print(f"Has Apple: {has_apple}")
+print(f"Has kiwi: {has_kiwi}")
+print(f"Length: {length}")
